fc7c55f6f4e9cb19ec4e07f3a0dcdfbd4ce885ac
gunis <jgunis@gmail.com>
2020-01-29 13:23:47 +0100

    generate DAD list for working week

---
+++ src/DadListGenerator.kt
@@ -0,0 +1,35 @@
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
+import java.util.Random
+
+val AVAILABLE_DUDES = listOf(
+    "Dude 1",
+    "Dude 2",
+    "Dude 3",
+    "Dude 4",
+    "Dude 5",
+    "Dude 6",
+    "Dude 7"
+)
+
+val WORKING_DAYS = listOf(
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+)
+
+private fun getCurrentDateTime() = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"))
+
+fun main() {
+
+    AVAILABLE_DUDES
+        .shuffled(Random())
+        .take(WORKING_DAYS.size)
+        .forEachIndexed { dayNumber, dude ->
+            println("${WORKING_DAYS[dayNumber]}: $dude")
+        }
+
+    println("\nGenerated on ${getCurrentDateTime()}")
+}
